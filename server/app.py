8e3cdf075ec42a46a56dd827168ad5d42aaf1554
doswald32 <dan.oswald32@gmail.com>
2025-01-04 00:50:34 -0700

    added route for /animals

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, make_response, jsonify
 from flask_restful import Resource
 
 # Local imports
@@ -15,6 +15,26 @@ from models import db, Animal
 @app.route('/')
 def index():
     return '<h1>Project Server</h1>'
+
+@app.route('/animals')
+def animals(): 
+
+    animals = []
+    for animal in Animal.query.all():
+        animal_dict = {
+            "id": animal.id,
+            "name": animal.name,
+            "DOB": animal.DOB,
+            "species": animal.species,
+        }
+        animals.append(animal_dict)
+
+        response = make_response(
+            jsonify(animals),
+            200
+        )
+
+        return response
 
 
 if __name__ == '__main__':
