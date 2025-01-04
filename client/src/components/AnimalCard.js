13458ac24446c9182781ef7bd1fef2a8f0e3665a
doswald32 <dan.oswald32@gmail.com>
2025-01-04 03:50:31 -0700

    updated formatting on date in AnimalCard component

--- client/src/components/AnimalCard.js
+++ client/src/components/AnimalCard.js
@@ -1,9 +1,12 @@
+import { format } from 'date-fns'
+
 function AnimalCard({ name, DOB, species}) {
+
     return (
         <div className="animal-card">
-            <h1>{name}</h1>
-            <p>{DOB}</p>
+            <h1>Name: {name}</h1>
             <p>{species}</p>
+            <span>{format(DOB, "MMMM do, yyyy")}</span>
         </div>
     )
 }
