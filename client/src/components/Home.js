920c7e7a4b5846928b39e0360d6789afa41ecad8
doswald32 <dan.oswald32@gmail.com>
2025-01-03 17:25:11 -0700

    added content to Home.js file

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,11 @@
 function Home() {
     return (
         <main>
-            <h1>Welcome!</h1>
+            <h1>Welcome to Dan's Vet Hospital!</h1>
+            <h3>Instructions</h3>
+            <p>- Click on the Dogs link to be taken to a list of dogs in our database</p>
+            <p>- Once in the Dogs section, click on a dog to see his/her information (Name, DOB, Owners, Visits, etc.)</p>
+            <p>- Click on Add a Visit to add a new vet visit for a dog</p>
         </main>
     )
 }
