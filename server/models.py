5779b1f253d888cc5ec52e350a26d8cfad528cb0
doswald32 <dan.oswald32@gmail.com>
2025-01-03 18:05:13 -0700

    created dog model, but having issues updating the schema

--- server/models.py
+++ server/models.py
@@ -1,6 +1,17 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from datetime import datetime
 
 from config import db
 
-# Models go here!
+class Dog(db.model):
+    __tablename__ = 'dogs'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    DOB = db.Column(db.DateTime)
+    species = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<Dog {self.id}, {self.name}, {self.DOB}, {self.species}>'
+
