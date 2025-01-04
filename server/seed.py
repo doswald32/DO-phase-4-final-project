1fd2b2ebfbe983d5b86680c34936f23f5a75dc89
doswald32 <dan.oswald32@gmail.com>
2025-01-03 23:22:32 -0700

    added to seed file. still working

--- server/seed.py
+++ server/seed.py
@@ -8,10 +8,21 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, Dog
+
+def create_dogs():
+    dogs = []
+    for _ in range(10):
+        dog = Dog(name=fake.name(), DOB=faker.date.between({ from: '2012-01-01', to: Date.now() })
+                  )
 
 if __name__ == '__main__':
+    
     fake = Faker()
+    
     with app.app_context():
-        print("Starting seed...")
-        # Seed code goes here!
+        print("Clearing database...")
+        Dog.query.delete()
+
+        print("Seeding dogs...")
+        dogs = 
