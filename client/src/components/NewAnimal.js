b5411184f2c4b47dc156e201f99e8081d1b58183
doswald32 <dan.oswald32@gmail.com>
2025-01-05 02:26:02 -0700

    new animal form is now stateful

--- client/src/components/NewAnimal.js
+++ client/src/components/NewAnimal.js
@@ -1,18 +1,53 @@
+import React, { useState } from "react";
+
 function NewAnimal() {
+
+    const [newAnimalData, setNewAnimalData] = useState({
+        name: "",
+        dob: "",
+        species: "",
+        owner_name: "",
+        last_visit: "",
+    });
+
+    function onChangeName(e) {
+        setNewAnimalData({ ...newAnimalData, name: e.target.value })
+        console.log(newAnimalData)
+    }
+
+    function onChangeDOB(e) {
+        setNewAnimalData({ ...newAnimalData, dob: e.target.value })
+        console.log(newAnimalData)
+    }
+
+    function onChangeSpecies(e) {
+        setNewAnimalData({ ...newAnimalData, species: e.target.value })
+    }
+
+    function onChangeOwnerName(e) {
+        setNewAnimalData({ ...newAnimalData, owner_name: e.target.value })
+    }
+
+    function onChangeLastVisit(e) {
+        setNewAnimalData({ ...newAnimalData, last_visit: e.target.value })
+    }
+
+    console.log(newAnimalData)
+
     return (
         <>
             <main>
                 <form className="new-animal-form">
                     <label>Name: </label>
-                    <input id="animal-form-name" className="animal-form-inputs" type="text" placeholder="Name"/>
+                    <input id="animal-form-name" className="animal-form-inputs" type="text" placeholder="Name" value={newAnimalData.name} onChange={onChangeName}/>
                     <label>DOB: </label>
-                    <input id="animal-form-dob" className="animal-form-inputs" type="date"/>
+                    <input id="animal-form-dob" className="animal-form-inputs" type="date" value={newAnimalData.dob} onChange={onChangeDOB}/>
                     <label>Species: </label>
-                    <input id="animal-form-species" className="animal-form-inputs" type="text" placeholder="Species"/>
+                    <input id="animal-form-species" className="animal-form-inputs" type="text" placeholder="Species" value={newAnimalData.species} onChange={onChangeSpecies}/>
                     <label>Owner Name: </label>
-                    <input id="animal-form-owner" className="animal-form-inputs" type="text" placeholder="Owner Name"/>
+                    <input id="animal-form-owner" className="animal-form-inputs" type="text" placeholder="Owner Name" value={newAnimalData.owner_name} onChange={onChangeOwnerName}/>
                     <label>Visit Date: </label>
-                    <input id="animal-form-visit" className="animal-form-inputs" type="date"/>
+                    <input id="animal-form-visit" className="animal-form-inputs" type="date" value={newAnimalData.last_visit} onChange={onChangeLastVisit}/>
                 </form>
             </main>
         </>
