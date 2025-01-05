df84a1ad9c9305762e61c290c840173fd901726a
doswald32 <dan.oswald32@gmail.com>
2025-01-05 00:16:06 -0700

    added inputs and styling to new animal form

--- client/src/components/NewAnimal.js
+++ client/src/components/NewAnimal.js
@@ -1,6 +1,21 @@
 function NewAnimal() {
     return (
-        <h1>Add an Animal!</h1>
+        <>
+            <main>
+                <form className="new-animal-form">
+                    <label>Name: </label>
+                    <input id="animal-form-name" className="animal-form-inputs" type="text" placeholder="Name"/>
+                    <label>DOB: </label>
+                    <input id="animal-form-dob" className="animal-form-inputs" type="date"/>
+                    <label>Species: </label>
+                    <input id="animal-form-species" className="animal-form-inputs" type="text" placeholder="Species"/>
+                    <label>Owner Name: </label>
+                    <input id="animal-form-owner" className="animal-form-inputs" type="text" placeholder="Owner Name"/>
+                    <label>Visit Date: </label>
+                    <input id="animal-form-visit" className="animal-form-inputs" type="date"/>
+                </form>
+            </main>
+        </>
     )
 }
 
