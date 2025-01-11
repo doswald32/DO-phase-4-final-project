2ee2f41bbb59b82bbdb6972f6c6ccd0a0817c4f0
doswald32 <dan.oswald32@gmail.com>
2025-01-10 23:25:08 -0700

    moved state for animalsList and associated setter function down to Animals.js file. Also moved the useEffect to fetch the data

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,15 @@
-import React, { useEffect, useState } from "react";
 import NavBar from "./NavBar";
 import "../App.css";
 import { Outlet } from "react-router-dom";
 
 function App() {
 
-  const [animalsList, setAnimalsList] = useState([])
-
-  useEffect(() => {
-    fetch('http://127.0.0.1:5555/animals')
-    .then(r => r.json())
-    .then(data => setAnimalsList(data))
-  }, [])
-
   return (
     <div>
       <header>
         <NavBar />
       </header>
-      <Outlet context={{ animalsList, setAnimalsList }}/>
+      <Outlet/>
     </div>
   );
 }
