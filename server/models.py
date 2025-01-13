d101cac34fc9ff90cc2a8b04e8a1e7e6344fa2f0
doswald32 <dan.oswald32@gmail.com>
2025-01-13 14:32:48 -0700

    modified models.py to reflect changes (Vets, Pets, and Visits)

--- server/models.py
+++ server/models.py
@@ -1,88 +1,59 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
-from datetime import datetime, date
 from sqlalchemy_serializer import SerializerMixin
 
 from config import db
 
-animal_owners = db.Table(
-    'animal_owners',
-    db.Column('animal_id', db.Integer, db.ForeignKey('animals.id'), primary_key=True),
-    db.Column('owner_id', db.Integer, db.ForeignKey('owners.id'), primary_key=True)
-)
+class Pet(db.Model, SerializerMixin):
+  __tablename__ = 'pets'
 
-class Animal(db.Model, SerializerMixin):
-    __tablename__ = 'animals'
+  serialize_rules = ('-visits.pet', '-vets.pets') 
 
-    serialize_rules = ('-visits.animal', '-owners.animals', '-vet.animals') 
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String)
+  dob = db.Column(db.Date)
+  species = db.Column(db.String)
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    dob = db.Column(db.Date)
-    species = db.Column(db.String)
-    vet_id = db.Column(db.Integer, db.ForeignKey('vets.id'))
+  visits = db.relationship('Visit', back_populates='pet', cascade='all, delete-orphan')
 
-    vet = db.relationship('Vet', back_populates='animals')
+  vets = association_proxy('visits', 'vet', creator=lambda vet_obj: Visit(vet=vet_obj))
 
-    visits = db.relationship('Visit', back_populates='animal', cascade='all, delete-orphan')
+  def __repr__(self):
+      return f'<Pet {self.id}, {self.name}, {self.dob}, {self.species}>'
 
-    owners = db.relationship('Owner', secondary=animal_owners, back_populates='animals')
-
-
-    def __repr__(self):
-        return f'<Animal {self.id}, {self.name}, {self.dob}, {self.species}>'
-
-
-class Owner(db.Model, SerializerMixin):
-    __tablename__ = 'owners'
-
-    serialize_rules = ('-visits.owner', '-animals.owners',)
-
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String)
-    last_name = db.Column(db.String)
-
-    visits = association_proxy('animals', 'visits')
-
-    animals = db.relationship('Animal', secondary=animal_owners, back_populates='owners')
-
-    def __repr__(self):
-        return f'<Owner {self.first_name} {self.last_name}>'
-    
 
 class Vet(db.Model, SerializerMixin):
-    __tablename__ = 'vets'
+  __tablename__ = 'vets'
 
-    serialize_rules = ('-animals.vet', '-visits.vet')
+  serialize_rules = ('-pets.vet', '-visits.vet')
 
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String)
-    last_name = db.Column(db.String)
-    hire_date = db.Column(db.Date)
+  id = db.Column(db.Integer, primary_key=True)
+  first_name = db.Column(db.String)
+  last_name = db.Column(db.String)
+  hire_date = db.Column(db.Date)
 
-    animals = db.relationship('Animal', back_populates='vet')
+  visits = db.relationship('Visit', back_populates='vet', cascade='all, delete-orphan')
 
-    visits = association_proxy('animals', 'visits')
+  pets = association_proxy('visits', 'pet', creator=lambda pet_obj: Visit(pet=pet_obj))
 
-    def __repr__(self):
-        return f'<Vet {self.id}, {self.first_name} {self.last_name}>'
-    
+  def __repr__(self):
+      return f'<Vet {self.id}, {self.first_name} {self.last_name} {self.hire_date}>'
+
 
 class Visit(db.Model, SerializerMixin):
-    __tablename__ = 'visits'
+  __tablename__ = 'visits'
 
-    serialize_rules = ('-animal.visits', '-owner.visits',)
+  serialize_rules = ('-pet.visits', '-vet.visits',)
 
-    id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.Date)
-    summary = db.Column(db.String)
+  id = db.Column(db.Integer, primary_key=True)
+  date = db.Column(db.Date)
+  summary = db.Column(db.String)
 
-    animal_id = db.Column(db.Integer, db.ForeignKey('animals.id'))
+  pet_id = db.Column(db.Integer, db.ForeignKey('pets.id'))
+  vet_id = db.Column(db.Integer, db.ForeignKey('vets.id'))
 
-    animal = db.relationship('Animal', back_populates='visits')
+  pet = db.relationship('Pet', back_populates='visits')
+  vet = db.relationship('Vet', back_populates='visits')
 
-    vet = association_proxy('animal', 'vet')
-
-    def __repr__(self):
-        return f'{self.date}'
-    
+  def __repr__(self):
+      return f'Visit {self.id}: Date: {self.date}, Summary: {self.summary} , Pet: {self.pet}, Vet {self.vet}'
